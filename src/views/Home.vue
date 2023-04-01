a70453ffbf755955c36a923054e82dcad9a1caf5
Laura Lo <rona576710@gmail.com>
2023-04-01 22:31:14 +0800

    github page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="welcomeScreen"/>
+    <BlogPost :post="post" v-for="(post,index) in sampleBlogPost"  :key="index"/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../assets/components/BlogPost.vue';
 export default {
-  name: "Home",
-  components: {},
+  name: "home",
+  components: {
+    BlogPost},
+  data(){
+    return{
+      welcomeScreen:{
+        title:"Welcome",
+        blogPost:"I'm blogpost",
+        welcomeScreen:true,
+        photo:"coding",
+      },
+      sampleBlogPost:[
+        {
+        title:"Filter Title",
+        blogHTML:"Filter Blog",
+        blogCoverPhoto:"beautiful-stories",
+      },{
+        title:"Filter Title",
+        blogHTML:"Filter Blog",
+        blogCoverPhoto:"designed-for-everyone",}
+      ]
+    }
+  }
 };
 </script>
